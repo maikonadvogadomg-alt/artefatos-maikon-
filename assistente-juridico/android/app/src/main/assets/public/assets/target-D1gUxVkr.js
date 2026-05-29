713fa8ab87be8d74c7b4641c10418a9431eec598
maikonadvogadomg-alt <maikon.advogado.mg@gmail.com>
2026-05-29 02:45:02 -0300

    Enviado pelo SK Code Editor - 29/05/2026

---
+++ assistente-juridico/android/app/src/main/assets/public/assets/target-D1gUxVkr.js
@@ -0,0 +1,11 @@
+import{q as c}from"./index-q_gTu9nj.js";/**
+ * @license lucide-react v0.453.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=c("Filter",[["polygon",{points:"22 3 2 3 10 12.46 10 19 14 21 14 12.46 22 3",key:"1yg77f"}]]);/**
+ * @license lucide-react v0.453.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=c("Target",[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["circle",{cx:"12",cy:"12",r:"6",key:"1vlfrh"}],["circle",{cx:"12",cy:"12",r:"2",key:"1c9p78"}]]);export{r as F,t as T};
