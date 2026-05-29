569ef03ebd258dfc7c67fd2ad2fe9e77a96815c3
maikonadvogadomg-alt <maikon.advogado.mg@gmail.com>
2026-05-29 02:45:07 -0300

    Enviado pelo SK Code Editor - 29/05/2026

---
+++ assistente-juridico/android/app/src/main/assets/public/assets/theme-toggle-Dbf3_SAN.js
@@ -0,0 +1,11 @@
+import{q as e,ac as o,I as t}from"./index-q_gTu9nj.js";import{B as h}from"./button-D5ZhSwEd.js";/**
+ * @license lucide-react v0.453.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const n=e("Moon",[["path",{d:"M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z",key:"a7tn18"}]]);/**
+ * @license lucide-react v0.453.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const m=e("Sun",[["circle",{cx:"12",cy:"12",r:"4",key:"4exip2"}],["path",{d:"M12 2v2",key:"tus03m"}],["path",{d:"M12 20v2",key:"1lh1kg"}],["path",{d:"m4.93 4.93 1.41 1.41",key:"149t6j"}],["path",{d:"m17.66 17.66 1.41 1.41",key:"ptbguv"}],["path",{d:"M2 12h2",key:"1t8f8n"}],["path",{d:"M20 12h2",key:"1q8mjw"}],["path",{d:"m6.34 17.66-1.41 1.41",key:"1m8zz5"}],["path",{d:"m19.07 4.93-1.41 1.41",key:"1shlcs"}]]);function d(){const{theme:a,toggleTheme:s}=o();return t.jsx(h,{size:"icon",variant:"ghost",onClick:s,"data-testid":"button-theme-toggle",children:a==="dark"?t.jsx(m,{className:"w-4 h-4"}):t.jsx(n,{className:"w-4 h-4"})})}export{d as T};
